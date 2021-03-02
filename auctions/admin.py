ca5f65658b12e18fb846d197e9164208fd25c7bc
Mellow-Developer <ghasemi7394@gmail.com>
2021-03-03 00:39:18 +0330

    final relations are done and this is the final commit

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,38 +1,18 @@
 from django.contrib import admin
-from .models import User, Bid, Comment, Category, Listing, Watchlist
+from .models import Comment, Listing
 
 # Register your models here.
 
 
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("id", "cat")
-
-
-class BidAdmin(admin.ModelAdmin):
-    list_display = ("id", "listing_id", "user_id", "bid", "active")
-
-
 class ListingAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "long_title", "description", "starting_price", "current_price", "image_url", "cats",
-                    "user", "date_created", "active")
-
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "username")
+    list_display = ("id", "title", "description", "current_price", "image_url", "date_created", "active")
 
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ("id", "listing_id", "user_id", "comment", "cm_time")
 
 
-class WatchlistAdmin(admin.ModelAdmin):
-    list_display = ("id", "listing_id", "user_id", "added")
-
-
 admin.site.register(Listing, ListingAdmin)
-admin.site.register(User, UserAdmin)
 admin.site.register(Comment, CommentAdmin)
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Bid, BidAdmin)
-admin.site.register(Watchlist, WatchlistAdmin)
+
 
